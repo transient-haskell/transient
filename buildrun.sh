5d4776df1542a6622c33fe3296c3076a1058fa47
Alberto G. Corona <agocorona@gmail.com>
2016-03-07 15:20:31 +0100

    test

--- buildrun.sh
+++ buildrun.sh
@@ -1,4 +1,4 @@
-sed -i -- "s/\"localhost\"/$1/g"  "./tests/Test.hs"
+sed -i -- "s/\"localhost\"/\"$1\"/g"  "./tests/Test.hs"
 ghcjs -isrc -i../ghcjs-hplay/src -i../ghcjs-perch/src tests/Test
 runghc -isrc -i../ghcjs-hplay/src -i../ghcjs-perch/src tests/Test
 
