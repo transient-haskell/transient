4fc3f10e19d6f812314f44a0ce11f45d3fa40f9e
Alberto G. Corona <agocorona@gmail.com>
2016-03-07 17:27:41 +0100

    test

--- tests/Test.hs
+++ tests/Test.hs
@@ -25,7 +25,7 @@ import Control.Monad.IO.Class
 
 
 main = do
-  let serverAddr= "127.0.0.1"
+  let serverAddr= "localhost"
       serverPort =  2020
       serverNode  = createNode serverAddr serverPort
       mynode    = if isBrowserInstance
