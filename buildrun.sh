63d62bdbdf95f9587b72ed1a42502f0cb367c24f
Alberto G. Corona <agocorona@gmail.com>
2016-03-07 12:51:16 +0100

    test

--- buildrun.sh
+++ buildrun.sh
@@ -1,3 +1,3 @@
 ghcjs -isrc -i../ghcjs-hplay/src -i../ghcjs-perch/src tests/Test
-runghc -isrc -i../ghcjs-hplay/src -i../ghcjs-perch/src tests/Test
+runghc -isrc -i../ghcjs-hplay/src -i../ghcjs-perch/src tests/Test $1
 
