e43669ef78b2bae81fad3b8bf177884cfa4ffacf
Alberto G. Corona <agocorona@gmail.com>
2016-04-05 20:24:21 +0200

    cleanup

--- buildrun.bat
+++
@@ -1,4 +0,0 @@
-ghcjs -isrc -i../ghcjs-hplay/src  %1 -o static/out
-if %errorlevel% neq 0 exit
-runghc -isrc -i../ghcjs-hplay/src %1
-
--- buildrun.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -e
-ghcjs -isrc -i../ghcjs-hplay/src  $1 -o static/out
-runghc -isrc -i../ghcjs-hplay/src $1
