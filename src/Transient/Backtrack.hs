235109f0658e2e2d5b39d3d4774679fe3fa3cb4b
Alberto G. Corona <agocorona@gmail.com>
2016-08-23 00:15:29 +0200

    backtracking code can inspect the reason of backtracking

--- src/Transient/Backtrack.hs
+++ src/Transient/Backtrack.hs
@@ -7,10 +7,10 @@ module Transient.Backtrack (onUndo, undo, retry, undoCut,registerUndo,
 
 -- * generalized versions of backtracking with an extra parameter that gives the reason for going back
 -- different kinds of backtracking with different reasons can be managed in the same program
-onBack, back, backCut,registerBack,
+onBack, back, forward, backCut,registerBack,
 
 -- * finalization primitives
-finish, onFinish, initFinish,killOnFinish,checkFinalize, FinishReason
+finish, onFinish, onFinish' ,initFinish , noFinish, killOnFinish ,checkFinalize , FinishReason
 ) where
 
 import Transient.Base
@@ -22,118 +22,132 @@ import Unsafe.Coerce
 import System.Mem.StableName
 import Control.Exception
 import Control.Concurrent.STM hiding (retry)
+import Data.Maybe
 
-data Backtrack b= Backtrack{backtracking :: Bool
-                           ,backStack :: [EventF]
-                           ,backReason :: b}
+data Backtrack b= Show b =>Backtrack{backtracking :: Maybe b
+                                    ,backStack :: [EventF] }
                                     deriving Typeable
 
 -- | assures that backtracking will not go further back
-backCut :: Typeable reason => reason -> TransientIO ()
+backCut :: (Typeable reason, Show reason) => reason -> TransientIO ()
 backCut reason= Transient $ do
-     delData $ Backtrack False [] reason
+     delData $ Backtrack (Just reason)  []
      return $ Just ()
 
 undoCut ::  TransientIO ()
 undoCut = backCut ()
 
 -- | the second parameter will be executed when backtracking
-{-# NOINLINE onUndo #-}
-onBack :: Typeable b => TransientIO a -> b -> TransientIO a -> TransientIO a
-onBack ac reason bac= registerBack reason $ Transient $ do
-     Backtrack back _ _ <- getData `onNothing` return (Backtrack False [] reason)
-     runTrans $ if back then bac  else ac
+{-# NOINLINE onBack #-}
+onBack :: (Typeable b, Show b) => TransientIO a -> ( b -> TransientIO a) -> TransientIO a
+onBack ac  bac= registerBack (typeof bac) $ Transient $ do
+     Backtrack mreason _  <- getData `onNothing` backStateOf (typeof bac)
+     runTrans $ case mreason of
+                  Nothing     -> ac
+                  Just reason -> bac reason
+     where
+     typeof :: (b -> TransIO a) -> b
+     typeof = undefined
 
 onUndo ::  TransientIO a -> TransientIO a -> TransientIO a
-onUndo x y= onBack x () y
+onUndo x y= onBack x (\() -> y)
 
 
 -- | register an action that will be executed when backtracking
 {-# NOINLINE registerUndo #-}
-registerBack :: Typeable b => b -> TransientIO a -> TransientIO a
-registerBack reason f  = Transient $ do
+registerBack :: (Typeable b, Show b) => b -> TransientIO a -> TransientIO a
+registerBack witness f  = Transient $ do
    cont@(EventF _ _ x _ _ _ _ _ _ _ _)  <- get   -- !!> "backregister"
 
-   md  <- getData `asTypeOf` (Just <$> backStateOf reason)
+   md <- getData `asTypeOf` (Just <$> backStateOf witness)
 
-   ss <- case md of
-        Just (bss@(Backtrack b (bs@((EventF _ _ x'  _ _ _ _ _ _ _ _):_))_)) -> do
-            addrx  <- addr x
-            addrx' <- addr x'         -- to avoid duplicate backtracking points
-            return $ if addrx == addrx' then bss else  Backtrack b (cont:bs) reason
-        Nothing ->  return $ Backtrack False [cont] reason
-   setData ss
+   case md of
+            Just (bss@(Backtrack b (bs@((EventF _ _ x'  _ _ _ _ _ _ _ _):_)))) ->
+               when (isNothing b) $ do
+                   addrx  <- addr x
+                   addrx' <- addr x'         -- to avoid duplicate backtracking points
+                   setData $ if addrx == addrx' then bss else  Backtrack mwit (cont:bs)
+            Nothing ->  setData $ Backtrack mwit [cont]
+
    runTrans f
    where
+   mwit= Nothing `asTypeOf` (Just witness)
    addr x = liftIO $ return . hashStableName =<< (makeStableName $! x)
 
 
 registerUndo :: TransientIO a -> TransientIO a
-registerUndo f= registerBack () f
+registerUndo f= registerBack ()  f
 
 -- | restart the flow forward from this point on
-retry :: Typeable b => b -> TransIO ()
-retry reason= Transient $ do
-    Backtrack _ stack _<- getData `onNothing` return (Backtrack False [] reason)
-    setData $ Backtrack False stack reason
+forward :: (Typeable b, Show b) => b -> TransIO ()
+forward reason= Transient $ do
+    Backtrack _ stack <- getData `onNothing`  (backStateOf reason)
+    setData $ Backtrack(Nothing `asTypeOf` Just reason)  stack
     return $ Just ()
+
+retry= forward ()
+
+noFinish= forward (FinishReason Nothing)
 
 -- | execute backtracking. It execute the registered actions in reverse order.
 --
 -- If the backtracking flag is changed the flow proceed  forward from that point on.
 --
---If the backtrack stack is finished or undoCut executed, `undo` will stop.
-back :: Typeable b => b -> TransientIO a
+-- If the backtrack stack is finished or undoCut executed, `undo` will stop.
+back :: (Typeable b, Show b) => b -> TransientIO a
 back reason = Transient $ do
-  bs <- getData  `onNothing` return nullBack            -- !!>"GOBACK"
+  bs <- getData  `onNothing`  backStateOf  reason           -- !!>"GOBACK"
   goBackt  bs
 
   where
-  nullBack= Backtrack False [] reason
-  goBackt (Backtrack _ [] _)= return Nothing                      -- !!> "END"
-  goBackt (Backtrack b (stack@(first@(EventF _ _ x fs _ _  _ _ _ _ _): bs)) _)= do
 
-        setData $ Backtrack True stack reason
-        mr <-  runClosure first                                 -- !!> "RUNCLOSURE"
-        Backtrack back _ _ <- getData `onNothing` return nullBack `asTypeOf` backStateOf reason
-                                                                -- !!>"END RUNCLOSURE"
+  goBackt (Backtrack _ [] )= return Nothing                      -- !!> "END"
+  goBackt (Backtrack b (stack@(first : bs)) )= do
+        (setData $ Backtrack (Just reason) stack)
+
+        mr <-  runClosure first                                  -- !> "RUNCLOSURE"
+
+        Backtrack back _ <- getData `onNothing`  backStateOf  reason
+                                                                 -- !> "END RUNCLOSURE"
         case back of
-           True ->  goBackt $ Backtrack True bs reason                 -- !!> "BACK AGAIN"
-           False -> case mr of
-                   Nothing -> return empty                      -- !!> "FORWARD END"
-                   Just x ->  runContinuation first x           -- !!> "FORWARD EXEC"
+           Nothing -> case mr of
+                   Nothing ->  return empty                      -- !> "FORWARD END"
+                   Just x  ->  runContinuation first x           -- !> "FORWARD EXEC"
+           justreason -> goBackt $ Backtrack justreason bs       -- !> ("BACK AGAIN",back)
 
-backStateOf reason= return  $ Backtrack False [] reason
+backStateOf :: (Monad m, Show a, Typeable a) => a -> m (Backtrack a)
+backStateOf reason= return $ Backtrack (Nothing `asTypeOf` (Just reason)) []
 
-undo ::  TransientIO a
+undo ::  TransIO a
 undo= back ()
 
 ------ finalization
 
-type FinishReason= Maybe SomeException
+newtype FinishReason= FinishReason (Maybe SomeException) deriving (Typeable, Show)
 
 -- | initialize the event variable for finalization.
 -- all the following computations in different threads will share it
 -- it also isolate this event from other branches that may have his own finish variable
-initFinish= backCut (Nothing :: FinishReason)
+initFinish= backCut (FinishReason Nothing)
 
 -- | set a computation to be called when the finish event happens
-onFinish :: (FinishReason ->TransIO ()) -> TransIO ()
-onFinish f= (return ()) `onBack` (Nothing :: FinishReason) $ do
-    Backtrack back _ reason <- getData `onNothing`  backStateOf (Nothing :: Maybe SomeException)
+onFinish :: ((Maybe SomeException) ->TransIO ()) -> TransIO ()
+onFinish f= onFinish' (return ()) f
+
+
+-- | set a computation to be called when the finish event happens this only apply for
+onFinish' ::TransIO a ->((Maybe SomeException) ->TransIO a) -> TransIO a
+onFinish' proc f= proc `onBack`   \(FinishReason reason) ->
     f reason
 
+
 -- | trigger the event, so this closes all the resources
-finish :: FinishReason -> TransIO ()
-finish reason= back reason
-
-
-
+finish :: Maybe SomeException -> TransIO a
+finish reason= back (FinishReason reason)
 
 
 -- | kill all the processes generated by the parameter when finish event occurs
 killOnFinish comp= do
-
    chs <- liftIO $ newTVarIO []
    onFinish $ const $ liftIO $ killChildren chs   -- !> "killOnFinish event"
    r <- comp
@@ -145,5 +159,5 @@ checkFinalize v=
            case v of
               SDone ->  finish Nothing >> stop
               SLast x ->  return x
-              SError e -> liftIO ( print e) >> finish Nothing >> stop
+              SError e -> liftIO ( print e) >> finish  Nothing >> stop
               SMore x -> return x
