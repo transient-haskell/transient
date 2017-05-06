5c2b30cb627eb907de77facc56fe241f95725991
Harendra Kumar <harendra.kumar@gmail.com>
2017-05-07 01:25:30 +0530

    Fix and update some documentation

--- src/Transient/Base.hs
+++ src/Transient/Base.hs
@@ -9,19 +9,22 @@
 -- Stability   :
 -- Portability :
 --
--- | See http://github.com/agocorona/transient
+-- | See the
+-- <https://github.com/transient-haskell/transient/wiki/Transient-tutorial tutorial>
+-- for an overview of concepts and examples.  Also see the
+-- <http://github.com/agocorona/transient readme> on the github repository.
 -----------------------------------------------------------------------------
 
 module Transient.Base(
 -- * The Monad
 TransIO(..), TransientIO
 -- * Running the monad
-,keep, keep', stop
+,keep, keep', stop, exit
 
--- * input
-,option, input, exit
+-- * Asynchronous standard input
+,option, input
 
--- * Asynchronous operations
+-- * Asynchronous IO operations
 ,async,waitEvents, spawn, parallel, sample
 ,react
 
